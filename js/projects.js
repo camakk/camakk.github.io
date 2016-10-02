4be3461140e9160d01bc49b0765a4dee4302a926
Cameron Akker <cakker@college.harvard.edu>
2016-10-02 15:39:29 -0400

    first stable build, no links
    need to set up database system still

--- js/projects.js
+++ js/projects.js
@@ -1,30 +1,3 @@
-class Polygon {
-  constructor(height, width) {
-    this.height=height;
-    this.width=width;
-  }
-
-  get area() {
-    return this.calcArea();
-  }
-
-  calcArea() {
-    return this.height * this.width;
-  }
-
-}
-
-function printPolygon() {
-  const square = new Polygon(10,10);
-  alert(square.area)
-}
-
-var grindlay {
-
-}
-
-
-
-function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
+$("#heythere").click(function() {
+  $("#heythere").hide();
+})
