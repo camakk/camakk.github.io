ba4b983d0e6940707f74f6efb9d0eb3af6628772
Cameron Akker <camakk@gmail.com>
2017-12-08 10:48:36 -0500

    overhaul with simplified format

--- js/footer.js
+++
@@ -1,6 +0,0 @@
-// footer.js
-// this document has all the JavaScript for the footer
-
-var footerhtml = '<div class="w3-row-padding">';
-footerhtml += '<p>© 2014-2017 Cameron Akker<br> Powered by <a href="http://www.w3schools.com/w3css/">w3.css</a></p>';
-footerhtml += '</div>';
